53cca9ea707f3f555d8a3e3f7768e624f563d953
Danton Nguyen <dantonnguyen88@gmail.com>
2017-02-24 02:38:19 +0000

    before sasssssy

---
+++ public/scripts/error.js
@@ -0,0 +1,19 @@
+$(function() {
+
+  $(document).click(function() {
+    $('#error-notifcation').fadeIn({
+      queue: false
+    }).animate({
+      top: '150px'
+    });
+  });
+
+
+  $('#error-notifcation').click(function() {
+    $(this).fadeOut({
+      queue: false
+    }).animate({
+      top: '140px'
+    });
+  });
+});
